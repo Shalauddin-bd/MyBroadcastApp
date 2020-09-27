6da476595e42bbe041a4acd20813d92e9d0a8bc2
Shalauddin-bd <mksbd.it@gmail.com>
2020-09-28 00:06:48 +0600

    Project add commit

---
+++ app/src/main/java/com/example/mybroadcastapp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.mybroadcastapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textViewSms;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewSms = findViewById(R.id.textViewSms);
+        requestSmsPermission();
+        Intent smsIntent = getIntent();
+        if(getIntent() != null)
+        {
+            Log.i("Main Activity","SMS received!");
+            textViewSms.setText(smsIntent.getStringExtra("data"));
+        }
+    }
+
+    private void requestSmsPermission() {
+        String permission = Manifest.permission.RECEIVE_SMS;
+        int grant = ContextCompat.checkSelfPermission(this, permission);
+        if ( grant != PackageManager.PERMISSION_GRANTED) {
+            String[] permission_list = new String[1];
+            permission_list[0] = permission;
+            ActivityCompat.requestPermissions(this, permission_list, 1);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/mybroadcastapp/MyReceiver.java
@@ -0,0 +1,39 @@
+package com.example.mybroadcastapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+    private static final String TAG = "SMSBroadcastReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Intent recieved: " + intent.getAction());
+
+        if (intent.getAction() == SMS_RECEIVED) {
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                Object[] pdus = (Object[])bundle.get("pdus");
+                final SmsMessage[] messages = new SmsMessage[pdus.length];
+                for (int i = 0; i < pdus.length; i++) {
+                    messages[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                }
+                if (messages.length > -1) {
+                    Log.i(TAG, "Message recieved: " + messages[0].getMessageBody());
+                    Toast.makeText(context,"Message Received :"+ messages[0].getMessageBody(),Toast.LENGTH_LONG).show();
+                    Intent smsIntent = new Intent(context,MainActivity.class);
+                    smsIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    smsIntent.putExtra("data",messages[0].getMessageBody());
+                    context.startActivity(smsIntent);
+                }
+            }
+        }
+    }
+
+}
